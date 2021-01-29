fc56e2f82ef8d6f53f42f14ab8f38f959e4267ed
Drew Weston <awfiscus@comcast.net>
2021-01-29 15:43:42 -0700

    added psuedo code to js file and added a test input bar to html

---
+++ script.js
@@ -0,0 +1,18 @@
+//When the user writes something in the input field, the data is saved to local storage when the save button is clicked
+//When the time block is passed then the block turns gray
+//When the time is on the current hour the time block is red
+//When the time is future the time block is green
+
+
+//Functions:
+
+//Need a function to equate if a time block value is equal to current time then determine its color
+
+//moment().format(LT) is current time in form of 3:26 PM
+
+//Need a function for storing input value into local storage
+
+//Event Listeners:
+
+//Need an event listener for save buttons to kick off function for storing input data into local storage
+
